56a498d3088c226ea28302940ad1293535cbd58b
Meghshyam Vaishnav <141837760+meghDev05@users.noreply.github.com>
2024-08-09 11:04:45 +0530

    App.js
    Playing with functional components

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-// import react from node modules
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-// create html element using reactjs
-
-const parent = React.createElement(
-    "div", 
-    {id:"parent"}, [
-    React.createElement("div", {id:"child"}, [
-            React.createElement("h1", {}, "I am h1 tag Hi, Hello, How are you?"),
-            React.createElement("h2", {}, "I am h2 tag")
-        ]),
-    React.createElement("div", {id:"child2"}, [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag, Hello, I'm fine! What about you?")
-        ]),
-    ]);
-
-console.log(parent); // object
-
+// JSX - HTML like or XML like syntax
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// Component Composition
+const Title = () =>{
+    return (<h1 id="heading">Namaste React using JSX</h1>);
+}; 
+
+const number = 1000;
+
+const HeadingComponent = () => (
+    <div>
+        <Title />
+        <h1>Hello React Functional Component {number}</h1>
+        {Title()}
+    </div>
+);
+
+root.render(<HeadingComponent />);
